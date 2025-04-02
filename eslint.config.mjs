7fbc26a17ac764da61d927b9523196ac982dea8d
Ravikaliya <1.ravi.kaliya@gmail.com>
2025-04-03 01:34:11 +0530

    chore: update ESLint configuration to disable additional rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,10 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+eslintConfig.push({
   rules: {
     'react/no-unescaped-entities': 'off',
     '@next/next/no-page-custom-font': 'off',
+    'no-console': 'off', // Example: Disables the 'no-console' rule
+    'react/prop-types': 'off', // Example: Disables the 'react/prop-types' rule
   },
-];
+});
 
 export default eslintConfig;
